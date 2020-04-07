c1ecda1c2ee33fecc42d333600abd1fa9c9c152f
Keno12 <1598136216@qq.com>
2020-04-07 17:20:30 +0800

    登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        登录页面
-    </div>
+  <div class="login-container">
+    <!-- ref 相当于 id，:model 表单数据对象, label-width 表单域标签的的宽度 -->
+    <el-form ref="form" :model="form" label-width="80px" class="login-form">
+      <h2 class="login-title">小刀会员管理系统</h2>
+      <el-form-item label="帐号">
+        <el-input v-model="form.username" placeholder="请输入帐号"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
+<script>
+export default {
+    data() {
+      return {
+        form: {
+         username:'',
+         password:''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.login-form {
+  width: 350px;
+  /* 上下间隙150px, 左右自动 */
+  margin: 160px auto;
+  /*透明背景色*/
+  background-color: rgb(255, 255, 255, 0.8);
+  padding: 30px;
+  /* 圆角 */
+  border-radius: 20px;
+}
+.login-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login.jpg");
+  overflow: hidden;
+}
+/* 标题 */
+.login-title {
+  text-align: center;
+  color: #303133;
+}
+</style>
