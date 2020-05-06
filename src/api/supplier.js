3a2cc5309b6e7874910dd97edfd437eeaf24eacd
Keno12 <1598136216@qq.com>
2020-05-06 21:08:19 +0800

    供应商管理-新增

--- src/api/supplier.js
+++ src/api/supplier.js
@@ -1,18 +1,25 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 export default {
     getList() {
         return request({
-            url: '/supplier/list',
-            method: 'get'
-        })
+            url: "/supplier/list",
+            method: "get"
+        });
     },
     search(page, size, searchMap) {
         return request({
             url: `/supplier/list/search/${page}/${size}`,
-            method: 'post',
+            method: "post",
             data: searchMap
-        })
+        });
+    },
+    // 新增
+    add(pojo) {
+        return request({
+            url: "/supplier",
+            method: "post",
+            data: pojo
+        });
     }
-
-}
+};
--- src/views/supplier/index.vue
+++ src/views/supplier/index.vue
@@ -1,31 +1,88 @@
 <template>
   <div>
-    <el-form ref="searchForm" :inline="true" :model="searchMap" style="margin-top: 20px;">
+    <el-form
+      ref="searchForm"
+      :inline="true"
+      :model="searchMap"
+      style="margin-top: 20px;"
+    >
       <el-form-item prop="name">
-        <el-input v-model="searchMap.name" placeholder="商品名称" style="width: 200px;"></el-input>
+        <el-input
+          v-model="searchMap.name"
+          placeholder="商品名称"
+          style="width: 200px;"
+        ></el-input>
       </el-form-item>
       <el-form-item prop="code">
-        <el-input v-model="searchMap.code" placeholder="商品编号" style="width: 200px;"></el-input>
+        <el-input
+          v-model="searchMap.code"
+          placeholder="商品编号"
+          style="width: 200px;"
+        ></el-input>
       </el-form-item>
       <el-form-item prop="supplierId">
-        <el-input v-model="searchMap.supplierName" placeholder="供应商" style="width: 200px;"></el-input>
+        <el-input
+          v-model="searchMap.supplierName"
+          placeholder="供应商"
+          style="width: 200px;"
+        ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search">查询</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+        >查询</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-edit"
+          @click="handleAdd"
+        >新增</el-button>
+        <el-button @click="resetForm('searchForm')">重置</el-button>
       </el-form-item>
     </el-form>
     <!-- 列表 -->
-    <el-table :data="list" height="380" border style="width: 100%">
+    <el-table
+      :data="list"
+      height="380"
+      border
+      style="width: 100%"
+    >
       <!--type="index" 获取索引值，从1开始； label 显示的标题; prop 数据字段名； width 列的宽度 -->
-      <el-table-column type="index" label="序号" width="60"></el-table-column>
-      <el-table-column prop="name" label="供应商名称"></el-table-column>
-      <el-table-column prop="linkman" label="联系人"></el-table-column>
-      <el-table-column prop="mobile" label="联系电话"></el-table-column>
-      <el-table-column prop="remark" label="备注"></el-table-column>
-      <el-table-column label="操作" width="150">
+      <el-table-column
+        type="index"
+        label="序号"
+        width="60"
+      ></el-table-column>
+      <el-table-column
+        prop="name"
+        label="供应商名称"
+      ></el-table-column>
+      <el-table-column
+        prop="linkman"
+        label="联系人"
+      ></el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="联系电话"
+      ></el-table-column>
+      <el-table-column
+        prop="remark"
+        label="备注"
+      ></el-table-column>
+      <el-table-column
+        label="操作"
+        width="150"
+      >
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.row.id)">编辑</el-button>
-          <el-button size="mini" type="danger" @click="handleDele(scope.row.id)">删除</el-button>
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.row.id)"
+          >编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDele(scope.row.id)"
+          >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -39,6 +96,61 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+
+    <el-dialog
+      title="供应商编辑"
+      :visible.sync="dialogFormVisible"
+      width="500px"
+    >
+      <!-- status-icon 当表单校验不通过时, 输入框右侧有个 x 小图标 -->
+      <el-form
+        :rules="rules"
+        status-icon
+        ref="pojoForm"
+        :model="pojo"
+        label-width="100px"
+        label-position="right"
+        style="width: 400px;"
+      >
+        <el-form-item
+          label="供应商名"
+          prop="name"
+        >
+          <el-input v-model="pojo.name" />
+        </el-form-item>
+
+        <el-form-item
+          label="联系电话"
+          prop="mobile"
+        >
+          <el-input v-model="pojo.mobile" />
+        </el-form-item>
+
+        <el-form-item
+          label="备注"
+          prop="remark"
+        >
+          <el-input
+            v-model="pojo.remark"
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4 }"
+            placeholder="请输入地址"
+          />
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="
+            pojo.id === null ? addData('pojoForm') : updateData('pojoForm')
+          "
+        >确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -55,6 +167,22 @@ export default {
         name: "",
         linkman: "",
         mobile: ""
+      },
+      pojo: {
+        id: null,
+        name: "",
+        linkman: "",
+        mobile: "",
+        remark: ""
+      },
+      dialogFormVisible: false,
+      rules: {
+        name: [
+          { required: true, message: "供应商名称不能为空", trigger: "blur" }
+        ],
+        linkman: [
+          { required: true, message: "联系人不能为空", trigger: "blur" }
+        ]
       }
     };
   },
@@ -85,8 +213,49 @@ export default {
     },
     handleDele(id) {
       console.log("删除", id);
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    addData(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 验证通过，提交添加
+          alert("Add submit!");
+        } else {
+          // 验证不通过
+          return false;
+        }
+      });
+    },
+    handleAdd() {
+      this.dialogFormVisible = true;
+      this.$nextTick(() => {
+        this.$refs["pojoForm"].resetFields();
+      });
+    },
+    addData(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 验证通过，提交添加
+          supplierApi.add(this.pojo).then(response => {
+            const resp = response.data;
+            if (resp) {
+              this.fetchData();
+              this.dialogFormVisible = false;
+            } else {
+              // 验证不通过
+              this.$message({
+                message: resp.message,
+                type: "warning"
+              });
+            }
+          });
+        } else {
+          return false;
+        }
+      });
     }
   }
 };
 </script>
-
